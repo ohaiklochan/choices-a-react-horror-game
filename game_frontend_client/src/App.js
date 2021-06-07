913dcf2e99e35364fb9c808dfded79fcd4611f40
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2021-06-07 16:58:43 -0500

    adding components to app

--- game_frontend_client/src/App.js
+++ game_frontend_client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Game from './components/game'
+import Choice from './components/choice'
+import Intro from './components/intro'
+import Ending from './components/ending'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Testing</h1>
+        <Game />
+        <Choice />
+        <Intro />
+        <Ending />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
