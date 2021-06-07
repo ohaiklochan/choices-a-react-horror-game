7b3a6bfc85e00257f77db0482fc1fc26e06900e8
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2021-06-07 15:56:40 -0500

    component addition

---
+++ game_frontend_client/src/components/ending.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Ending extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+export default Ending
