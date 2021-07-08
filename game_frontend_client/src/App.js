8b26be91970512b1df49e3ae6a90db8be4f45611
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2021-07-08 18:58:06 -0500

    addition of buzzfeed layout

--- game_frontend_client/src/App.js
+++ game_frontend_client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import Game from './components/game'
 import Choice from './components/choice'
-// import Intro from './components/intro'
-// import Ending from './components/ending'
+import Intro from './components/intro'
+import Ending from './components/ending'
+import { BuzzFeedQuiz } from "react-buzzfeed-quiz"
 import "./css/game.css"
+import "react-buzzfeed-quiz/lib/styles.css"
 
 class App extends Component {
 state = {
@@ -20,46 +22,8 @@ state = {
   lose: false
 }
 
-goToChoice = () => {
-  this.setState({
-    gameStarted: true
-  })
-}
-
-changePath = (choices, choice) => {
-  if (choices.choiceA == choice) {
-    const options = this.state.choices.filter(choice => choice.turn == this.state.turn)
-    const filteredOptions = options.filter(choice => choice.route == this.state.route)
-    const reFilteredOptions = filteredOptions.filter(choice => choice.from == this.state.prompt)
-    const newPath = reFilteredOptions.find(choice => choice.path == "left")
-    const newerPath = reFilteredOptions.filter(choice => choice.path == "left")
-    const choice = this.state.choices[0].path
-    if(this.state.route == "none") {
-      this.setState({
-        prompt: newPath.prompt,
-        choiceA: newPath.choiceA,
-        choiceB: newPath.choiceB,
-        turn: this.state.turn + 1,
-        route: "Good"
-      })
-    }
-    
-  }
-}
-
   render() {
-    const playing = this.state.gameStarted
-    return (
-      <div>
-        {this.state.gameStarted ? <Choice prompt={this.state.prompt} choiceA={this.state.choiceA} choiceB={this.state.choiceB} changePath={this.changePath} /> :
-        <div className="background">
-          <button onClick={this.goToChoice}>Start</button>
-            <div className="description">Choices: A React Horror Game</div>
-            <p>Do you have what it takes to survive the night?</p>
-          </div>
-        }
-  </div>
-    )}
+    return }
 }
 
 export default App
--- game_frontend_client/src/index.js
+++ game_frontend_client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './css/game.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
